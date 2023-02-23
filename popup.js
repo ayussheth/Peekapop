7516fa5867823b043dc4df9b5a62d6e75df0af9a
Ayussh <ayusshkhurana7@gmail.com>
2023-02-23 22:59:12 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,31 +1,39 @@
 // Listen for the popup to be shown
 document.addEventListener('DOMContentLoaded', function() {
-  // Add a mouseover event listener to the document
-  document.addEventListener('mouseover', function(event) {
-    // Check if the event target is a link
-    if (event.target.tagName === 'A') {
-      // Get the link URL
-      const url = event.target.href;
-      // Create an iframe element with the link URL as the source
-      const iframe = document.createElement('iframe');
-      iframe.src = url;
-      // Set the size and position of the iframe
-      iframe.style.width = '100%';
-      iframe.style.height = '100%';
-      iframe.style.position = 'absolute';
-      iframe.style.top = '0';
-      iframe.style.left = '0';
-      // Add the iframe to the popup window
-      document.body.appendChild(iframe);
-    }
-  });
-
-  // Add a mouseout event listener to the document
-  document.addEventListener('mouseout', function(event) {
-    // Check if the event target is a link
-    if (event.target.tagName === 'A') {
+  // Find all links on the page
+  const links = document.getElementsByTagName('a');
+  // Loop through each link
+  for (let i = 0; i < links.length; i++) {
+    const link = links[i];
+    let timer = null;
+    // Attach a mousedown event listener to each link
+    link.addEventListener('mousedown', function() {
+      // Set a timer to display the preview after 500ms
+      timer = setTimeout(function() {
+        // Get the link URL
+        const url = link.href;
+        // Create an iframe element with the link URL as the source
+        const iframe = document.createElement('iframe');
+        iframe.src = url;
+        // Set the size and position of the iframe
+        iframe.style.width = '100%';
+        iframe.style.height = '100%';
+        iframe.style.position = 'absolute';
+        iframe.style.top = '0';
+        iframe.style.left = '0';
+        // Add the iframe to the popup window
+        document.body.appendChild(iframe);
+      }, 500);
+    });
+    // Attach a mouseup event listener to each link
+    link.addEventListener('mouseup', function() {
+      // Clear the timer if the user releases the mouse button before the timer expires
+      clearTimeout(timer);
       // Remove the iframe from the popup window
-      document.body.removeChild(document.querySelector('iframe'));
-    }
-  });
+      const iframe = document.querySelector('iframe');
+      if (iframe) {
+        document.body.removeChild(iframe);
+      }
+    });
+  }
 });
